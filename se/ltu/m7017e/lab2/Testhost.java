d75db263f10f9aa28c560bbef1e1924d0f5adcb9
krz37j <jerry.henry@gmx.us>
2015-11-24 14:58:30 +0100

    2 way

--- se/ltu/m7017e/lab2/Testhost.java
+++ se/ltu/m7017e/lab2/Testhost.java
@@ -12,9 +12,18 @@ class Testhost {
 		Gst.init("Receiving", args);
 		Pipeline hostPipeline;
 		int guest1RecvPort=7777;
-	
+		int guest1SendPort=6666;
+		int guest1Address="to be added";
 
 		String caps = "audio/x-raw-int, channels=1, rate=8000, width=16, depth=16, endianness=1234, signed=true";
+
+		String audioFromHost = String.format(
+				"autoaudiosrc ! %s ! tee name=fromhost ! queue !", caps);
+
+		String audioToGuest1 = String.format(
+				"liveadder name=toguest1 ! mulawenc ! rtppcmupay ! queue ! "
+						+ "udpsink host=%s port=%d async=false sync=false",
+				guest1Address, guest1SendPort);
 
 		String audioFromGuest1 = String
 				.format("udpsrc port=%d caps=\"application/x-rtp\" ! queue ! rtppcmudepay ! mulawdec ! "
@@ -22,7 +31,7 @@ class Testhost {
 						guest1RecvPort, caps);
 		String audioToHost = "! queue ! autoaudiosink";
 	
-		String pipeline = String.format("%s %s ",
+		String pipeline = String.format("%s %s %s %s",audioFromHost, audioToGuest1,
 				audioFromGuest1, audioToHost);
 
 		// And so, we finally create the pipeline.
